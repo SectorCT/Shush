0b14acd8c9b155f7b7ecc08dfcdc22930456eb64
Svetoslav47 <svetoslav2806@gmail.com>
2023-03-09 13:03:33 +0200

    Create styles.js

---
+++ client/styles.js
@@ -0,0 +1,16 @@
+colors = {
+    primary: '#3F51B5',
+    secondary: '#FF4081',
+    accent: '#FFC107',
+}
+
+fonts = {
+    primary: 'Roboto, sans-serif',
+    secondary: 'Roboto Mono, monospace',
+}
+
+
+export default {
+    colors,
+    font,
+};
