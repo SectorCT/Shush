3458ca29a16dff7515e0814ebebf1c6dc0d0b33f
Svetoslav47 <svetoslav2806@gmail.com>
2023-03-17 03:11:47 +0200

    delete chatback

--- chatback/room/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()),
-]
